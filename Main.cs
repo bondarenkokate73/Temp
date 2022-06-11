a17b3e34c363e824fea6fb9053bb4bfa29c78cb9
bondarenkokets <bondarenkokets@yandex.ru>
2022-06-11 14:33:33 +0300

    Добавьте файлы проекта.

---
+++ Main.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Temp
+{
+    public partial class Main : Form
+    {
+        static readonly Random random = new Random();
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            await clickLeftOnPointAsync(new Point(900, 200));
+            await clickLeftOnPointAsync(new Point(1000, 300));
+            await clickLeftOnPointAsync(new Point(1100, 400));
+        }
+
+        public static Task Wait()
+        {
+            int sleep = random.Next(300, 600);
+            return Task.Delay(sleep);
+        }
+
+        public async Task clickLeftOnPointAsync(Point point)
+        {
+            if (!point.IsEmpty)
+            {
+                int x = Convert.ToInt32(point.X / 1.25);
+                int y = Convert.ToInt32(point.Y / 1.25);
+
+                Point offsetPoint = new Point(x + 5, y + 5);
+
+                await ClickLeftMouseButtonAsync(offsetPoint.X, offsetPoint.Y);
+                await Wait();
+            }
+        }
+
+        public static async Task ClickLeftMouseButtonAsync(int x, int y)
+        {
+            x = (x * 65536) / WinApi.GetSystemMetrics(WinApi.SystemMetric.SM_CXSCREEN);
+            y = (y * 65536) / WinApi.GetSystemMetrics(WinApi.SystemMetric.SM_CYSCREEN);
+
+            WinApi.INPUT[] inputs = new WinApi.INPUT[2];
+
+            inputs[0] = new WinApi.INPUT
+            {
+                type = WinApi.SendInputEventType.InputMouse,
+                mkhi = new WinApi.MouseKeybdhardwareInputUnion
+                {
+                    mi = new WinApi.MouseInputData
+                    {
+                        dx = x,
+                        dy = y,
+                        dwFlags = WinApi.MouseEventFlags.MOUSEEVENTF_MOVE | WinApi.MouseEventFlags.MOUSEEVENTF_ABSOLUTE
+                    }
+                }
+            };
+
+            inputs[1] = new WinApi.INPUT
+            {
+                type = WinApi.SendInputEventType.InputMouse,
+                mkhi = new WinApi.MouseKeybdhardwareInputUnion
+                {
+                    mi = new WinApi.MouseInputData
+                    {
+                        dx = x,
+                        dy = y,
+                        dwFlags = WinApi.MouseEventFlags.MOUSEEVENTF_LEFTDOWN
+                    }
+                }
+            };
+            WinApi.SendInput((uint)inputs.Length, inputs, Marshal.SizeOf(typeof(WinApi.INPUT)));
+            await Wait();
+
+            WinApi.INPUT iNPUT = new WinApi.INPUT
+            {
+                type = WinApi.SendInputEventType.InputMouse,
+                mkhi = new WinApi.MouseKeybdhardwareInputUnion
+                {
+                    mi = new WinApi.MouseInputData
+                    {
+                        dx = x,
+                        dy = y,
+                        dwFlags = WinApi.MouseEventFlags.MOUSEEVENTF_LEFTUP
+                    }
+                }
+            };
+            WinApi.SendInput(1, ref iNPUT, Marshal.SizeOf(typeof(WinApi.INPUT)));
+        }
+    }
+}
---
+++ WinApi.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Temp
+{
+    class WinApi
+    {
+        [DllImport("user32.dll")]
+        public static extern uint SendInput(uint nInputs, INPUT[] pInputs, int cbSize);
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern uint SendInput(uint nInputs, ref INPUT pInputs, int cbSize);
+        [DllImport("user32.dll")]
+        public static extern int GetSystemMetrics(SystemMetric smIndex);
+
+        public enum SystemMetric
+        {
+            SM_CXSCREEN = 0,
+            SM_CYSCREEN = 1,
+        }
+
+        [Flags]
+        public enum MouseEventFlags : uint
+        {
+            MOUSEEVENTF_MOVE = 0x0001,
+            MOUSEEVENTF_LEFTDOWN = 0x0002,
+            MOUSEEVENTF_LEFTUP = 0x0004,
+            MOUSEEVENTF_RIGHTDOWN = 0x0008,
+            MOUSEEVENTF_RIGHTUP = 0x0010,
+            MOUSEEVENTF_MIDDLEDOWN = 0x0020,
+            MOUSEEVENTF_MIDDLEUP = 0x0040,
+            MOUSEEVENTF_XDOWN = 0x0080,
+            MOUSEEVENTF_XUP = 0x0100,
+            MOUSEEVENTF_WHEEL = 0x0800,
+            MOUSEEVENTF_VIRTUALDESK = 0x4000,
+            MOUSEEVENTF_ABSOLUTE = 0x8000
+        }
+        public enum SendInputEventType : int
+        {
+            InputMouse,
+            InputKeyboard,
+            InputHardware
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct INPUT
+        {
+            public SendInputEventType type;
+            public MouseKeybdhardwareInputUnion mkhi;
+        }
+
+        public struct MouseInputData
+        {
+            public int dx;
+            public int dy;
+            public uint mouseData;
+            public MouseEventFlags dwFlags;
+            public uint time;
+            public IntPtr dwExtraInfo;
+        }
+
+        [StructLayout(LayoutKind.Explicit)]
+        public struct MouseKeybdhardwareInputUnion
+        {
+            [FieldOffset(0)]
+            public MouseInputData mi;
+        }
+    }
+}
